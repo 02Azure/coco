7fdfd88570c9fd854a04e89d02dfe7a1127322a6
nasrullahaufa <nasrullahaufa@gmail.com>
2021-05-28 15:06:44 +0700

    tdd baru register login

---
+++ server/app_test.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("routed");
+});
+
+app.post("/login", postLogin);
+app.post("/register", postRegister);
+
+// app.listen(PORT, () => {
+//   console.log(`idling at ${PORT} RPM`);
+// });
+
+module.exports = app;
+
+function postRegister(req, res, next) {
+  console.log(req.body);
+
+  if (req.body.email == "registered@mail.com") {
+    res.status(400).json({ message: "Email is already used" });
+  }
+  if (req.body.email == "usermail.com") {
+    res.status(400).json({ message: "Incorrect email format" });
+  }
+  if (req.body.password.length < 6) {
+    res.status(400).json({ message: "Password length 6 character minimum" });
+  } else {
+    res.status(201).json({
+      username: req.body.username,
+      email: req.body.email,
+      id: 1,
+    });
+  }
+}
+
+function postLogin(req, res, next) {
+  if (req.body.email == "usermail.com") {
+    res.status(401).json({"message" :"Incorrect email or password"})
+  }else{
+    res.status(200).json({
+      id: 1,
+      email: req.body.email,
+      access_token:'access_token'
+    })
+  }
+
+}
---
+++ server/test/user.test.js
@@ -0,0 +1,195 @@
+const request = require("supertest");
+// const { hashPassword } = require("../helpers/bcrypt");
+
+const app = require("../app_test");
+// const { sequelize } = require("../models");
+// const { queryInterface } = sequelize;
+let successCase = {
+  username: "user",
+  email: "user@mail.com",
+  password: "123456",
+};
+let registered = {
+  email: "registered@mail.com",
+};
+
+let wrongFormat = {
+  username: "user",
+  email: "usermail.com",
+  password: "123456",
+};
+
+let badPassword = {
+  username: "user",
+  email: "user@mail.com",
+  password: "1256",
+};
+
+describe("POST register user sukses", () => {
+  it("it responds with status 200 ,username ,email, id", (done) => {
+    request(app)
+      .post("/register")
+      .send(successCase)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        let { body, status } = response;
+        expect(status).toBe(201);
+        console.log(body);
+        expect(body).toHaveProperty("id", expect.any(Number));
+        expect(body).toHaveProperty("email", successCase.email);
+        expect(body).toHaveProperty("username", successCase.username);
+        done();
+      })
+      .catch((err) => {
+        // console.log(err)
+        done(err);
+      });
+  });
+});
+describe("POST register user dengan email yg sudah terdaftar", () => {
+  it("it responds with status 400 'Email is already used", (done) => {
+    request(app)
+      .post("/register")
+      .send(registered)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        let { body, status } = response;
+        expect(status).toBe(400);
+        console.log(body);
+        expect(body).toHaveProperty("message", "Email is already used");
+        done();
+      })
+      .catch((err) => {
+        // console.log(err)
+        done(err);
+      });
+  });
+});
+describe("POST register dgn format email salah", () => {
+  it('it responds with msg "Incorrect email format"', (done) => {
+    request(app)
+      .post("/register")
+      .send(wrongFormat)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        let { body, status } = response;
+        // console.log(body)
+        expect(status).toBe(400);
+        expect(body).toHaveProperty("message", "Incorrect email format");
+        done();
+      })
+      .catch((err) => {
+        // console.log(err)
+        done(err);
+      });
+  });
+});
+describe("POST register dgn password kurang dari 6 karakter", () => {
+  it('it responds with msg "Password length 6 character minimum"', (done) => {
+    request(app)
+      .post("/register")
+      .send(badPassword)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        let { body, status } = response;
+        // console.log(body)
+        expect(status).toBe(400);
+        expect(body).toHaveProperty(
+          "message",
+          "Password length 6 character minimum"
+        );
+        done();
+      })
+      .catch((err) => {
+        // console.log(err)
+        done(err);
+      });
+  });
+});
+describe("POST login user sukses", () => {
+  it("it responds with status 200 ,email, id, access_token", (done) => {
+    request(app)
+      .post("/login")
+      .send(successCase)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        let { body, status } = response;
+        expect(status).toBe(200);
+        console.log(body);
+        expect(body).toHaveProperty("id", expect.any(Number));
+        expect(body).toHaveProperty("email", successCase.email);
+        expect(body).toHaveProperty("access_token", expect.any(String));
+        done();
+      })
+      .catch((err) => {
+        // console.log(err)
+        done(err);
+      });
+  });
+});
+describe("POST login customer dgn email/password salah", () => {
+  it('it responds with status 401 and message :"Incorrect email or password"', (done) => {
+    request(app)
+      .post("/login")
+      .send(wrongFormat)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        let { body, status } = response;
+        // console.log(body)
+        expect(status).toBe(401);
+        expect(body).toHaveProperty("message", "Incorrect email or password");
+        done();
+      })
+      .catch((err) => {
+        // console.log(err)
+        done(err);
+      });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// beforeAll((done) => {
+//     queryInterface
+//       .bulkInsert("Users", [
+//         {
+//           email: "admin@mail.com",
+//           password: hashPassword("123456"),
+//           isAdmin: true,
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//         },
+//       ])
+//       .then(() => {
+//         done();
+//       })
+//       .catch((err) => {
+//         done(err);
+//       });
+//   });
+
+//   afterAll((done) => {
+//     queryInterface
+//       .bulkDelete("Users")
+//       .then(() => {
+//         done();
+//       })
+//       .catch((err) => {
+//         done(err);
+//       });
+//   });
