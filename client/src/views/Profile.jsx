0addb748260af439c4e7c81791b2144e6b5f792f
boby bewa <bobybewa6@mail.com>
2021-05-28 20:04:56 +0700

    stling profile page

--- client/src/views/Profile.jsx
+++ client/src/views/Profile.jsx
@@ -1,64 +1,77 @@
 import React from "react";
-
 import "./profile.css";
-
-import image from ".././images/bg_1.jpg";
-
 import oke from ".././images/002.png";
 
 export const Profile = () => {
   return (
     <section className="profile">
+      {/* navbar image */}
       <div className="header__image__container">
-        <img src={image} alt="" className="header__image" />
+        <a href="#" className="navbar__anchor">Home</a>
       </div>
 
+      {/* profile side */}
       <div className="profile__container row">
         {/* user info */}
-
         <div className="user__container col-md-4 p-3">
-          <div>
-            <img src={oke} alt="" className="header__image" />
-          </div>
-
-          <div className="username">
-            <p>username</p>
-          </div>
-
-          <div className="bio">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo iste pariatur explicabo aperiam. Dolores, quia!</p>
-          </div>
-
-          <div className="location">
-            <p>location</p>
+          <div className="user__info__container">
+            <div className="content__image" >
+              <img src={oke} alt="" className="header__image" />
+            </div>
+            <div className="username">
+              <p className="username__text">@username</p>
+            </div>
+            <div className="main__content">
+              <p className="text">Full Stack Javascript Developer</p>
+              <p className="text">location</p>
+            </div>
+            <button className="btn__chat">Chat</button>
           </div>
         </div>
 
         <div className="showcase col-md-8 p-3">
           {/* button2 */}
           <div className="buttons">
-            <button className="btn">shocse</button>
-
-            <button className="btn">items</button>
-
-            <button className="btn">watchlists</button>
-
-            <button className="btn">add shocase</button>
+            <a className="btn">show case</a>
+            <a className="btn">items</a>
+            <a className="btn">wishlist</a>
+          <a href="#" className="add__showCase">
+            <img className="imgCase" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik0xMSAxMXYtMTFoMXYxMWgxMXYxaC0xMXYxMWgtMXYtMTFoLTExdi0xaDExeiIvPjwvc3ZnPg=="/>
+          </a>
           </div>
 
           {/* list shocases */}
 
           <div className="showcase__container">
-            <div className="showcase__buttons">
-              <h4 className="showcase__name">title</h4>
-
-              <a className="see__all">see all</a>
-            </div>
-
             <div className="items__container">
-              <div>
-                <h5>name</h5>
-
+              <div className="items__images">
+                <h5>judul 1</h5>
+                <a className="see__all">see all</a>
+                <img src={oke} alt="" className="item__image" />
+                <img src={oke} alt="" className="item__image" />
+              </div>
+              <div className="items__images">
+                <h5>judul 2</h5>
+                <a className="see__all">see all</a>
+                <img src={oke} alt="" className="item__image" />
+                <img src={oke} alt="" className="item__image" />
+              </div>
+              <div className="items__images">
+                <h5>judul 3</h5>
+                <a className="see__all">see all</a>
+                <img src={oke} alt="" className="item__image" />
+                <img src={oke} alt="" className="item__image" />
+              </div>
+              <div className="items__images">
+                <h5>judul 4</h5>
+                <a className="see__all">see all</a>
+                <img src={oke} alt="" className="item__image" />
+                <img src={oke} alt="" className="item__image" />
+              </div>
+              <div className="items__images">
+                <h5>judul 5</h5>
+                <a className="see__all">see all</a>
+                <img src={oke} alt="" className="item__image" />
                 <img src={oke} alt="" className="item__image" />
               </div>
             </div>
