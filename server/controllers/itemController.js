2df44df30e1874d1288479777d20197c202c9e65
02Azure <agusdomas1st@gmail.com>
2021-05-29 17:55:26 +0700

    minor update itemcontroller

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -13,11 +13,8 @@ class itemController{
     static async getItemById(req, res, next){
         try {
             const item = await Item.findByPk(req.params.id)
-            if(!item){
-                throw { status: 404, message: 'Item is not found!' }
-            } else {
-                res.status(200).json(item)
-            }
+            res.status(200).json(item)
+
         } catch (err) {
             next(err)
         }
@@ -38,14 +35,9 @@ class itemController{
     static async putItem(req, res, next){
         try {
             const { name, image, tradeable, price, tradeWith, tag, description } = req.body
-            const findItem = await Item.findByPk(req.params.id)
+            const updateItem = await Item.update({ name, image, tradeable, price, tradeWith, tag, description }, { where: { id: +req.params.id }})
+            res.status(200).json({ msg: "Item has been successfully updated" })
 
-            if(!findItem){
-                throw { status: 404, message: 'Item is not found' }
-            } else {
-                const updateItem = await Item.update({ name, image, tradeable, price, tradeWith, tag, description }, { where: { id: findItem.id }})
-                res.status(200).json({ msg: "Item has been successfully updated" })
-            }
         } catch (err) {
             next(err)
         }
@@ -54,13 +46,8 @@ class itemController{
     static async patchItem(req, res, next){
         try {
             const { tradeable } = req.body
-            const findItem = await Item.findByPk(req.params.id) 
-            if(!findItem){
-                throw { status: 404, message: 'Item is not found' }
-            } else {
-                const patchItem = await Item.update({ tradeable }, { where: { id: findItem.id }})
-                res.status(200).json({ msg: "Item tradeable has been successfully updated" })
-            }
+            await Item.update({ tradeable }, { where: { id: +req.params.id }})
+            res.status(200).json({ msg: "Item tradeable has been successfully updated" })
         } catch (err) {
             
         }
@@ -69,13 +56,9 @@ class itemController{
     static async deleteItem(req, res, next){
         try {
             const findItem = await Item.findByPk(req.params.id)
+            await Item.destroy({ where: { id: findItem.id }})
+            res.status(200).json({ msg: 'Item has been successfully deleted' })
 
-            if(!findItem){
-                throw { status: 404, message: 'Item is not found' }
-            } else {
-                await Item.destroy({ where: { id: findItem.id }})
-                res.status(200).json({ msg: 'Item has been successfully deleted' })
-            }
         } catch (err) {
             next(err)
         }
