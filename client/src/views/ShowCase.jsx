a43ef8ff2ae316d798ac9e004a0f6856c9298e5d
fahmi-1997 <fahmialamsyah97@gmail.com>
2021-05-30 22:42:31 +0700

    seeall

--- client/src/views/ShowCase.jsx
+++ client/src/views/ShowCase.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import oke from ".././images/002.png";
 
 const ShowCase = () => {
+  const [all, setAll] = useState(false);
   return (
     <div className="items__images m-1 d-flex flex-column">
       <div className="d-flex justify-content-between p-2 align-items-start">
         <h5>judul 1</h5>
-        <a className="">see all</a>
+        <a onClick={() => setAll(!all)}>see all</a>
       </div>
 
-      <div className="d-flex">
-        <div className="items__showcase p-2 m-1">
-          <img src={oke} alt="" className="item__image" />
+      {!all ? (
+        <div className="row justify-content-center">
+          <div className="col-md-4 mx-auto">
+            <img src={oke} alt="" className="item__image" />
+          </div>
+          <div className="col-md-4">
+            <img src={oke} alt="" className="item__image" />
+          </div>
+          <div className="col-md-4">
+            <img src={oke} alt="" className="item__image" />
+          </div>
         </div>
-        <div className="items__showcase p-2 m-1">
-          <img src={oke} alt="" className="item__image" />
+      ) : (
+        <div className="row justify-content-center">
+          <div className="col-md-4 mx-auto">
+            <img src={oke} alt="" className="item__image" />
+          </div>
+          <div className="col-md-4">
+            <img src={oke} alt="" className="item__image" />
+          </div>
+          <div className="col-md-4">
+            <img src={oke} alt="" className="item__image" />
+          </div>
+          <div className="col-md-4">
+            <img src={oke} alt="" className="item__image" />
+          </div>
+          <div className="col-md-4">
+            <img src={oke} alt="" className="item__image" />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
